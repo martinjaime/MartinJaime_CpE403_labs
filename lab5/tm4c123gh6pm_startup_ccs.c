a27b4bc0aad51c8952c5269abb961960e0c4f122
Martin <jaimem5@unlv.nevada.edu>
2015-10-23 21:50:17 -0700

    Completed Lab 5

--- lab5/tm4c123gh6pm_startup_ccs.c
+++ lab5/tm4c123gh6pm_startup_ccs.c
@@ -41,6 +41,7 @@ static void IntDefaultHandler(void);
 //
 //*****************************************************************************
 extern void _c_int00(void);
+extern void IntTimer0Handler(void);
 
 //*****************************************************************************
 //
@@ -102,7 +103,8 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // ADC Sequence 2
     IntDefaultHandler,                      // ADC Sequence 3
     IntDefaultHandler,                      // Watchdog timer
-    IntDefaultHandler,                      // Timer 0 subtimer A
+    //IntDefaultHandler,                      // Timer 0 subtimer A
+	IntTimer0Handler,                       // Timer 0 subtimer A
     IntDefaultHandler,                      // Timer 0 subtimer B
     IntDefaultHandler,                      // Timer 1 subtimer A
     IntDefaultHandler,                      // Timer 1 subtimer B
