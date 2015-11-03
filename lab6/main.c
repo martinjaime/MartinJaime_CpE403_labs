47cfbc61800c62b09781ba1a28987973e994a47c
Martin <jaimem5@unlv.nevada.edu>
2015-11-03 11:25:36 -0800

    Completed Lab 06

--- lab6/main.c
+++ lab6/main.c
@@ -27,7 +27,7 @@ int main()
 	// Enable LEDs as outputs and turn on green LED.
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
-	GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x08);
+	//GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x08);
 
 	// Enable hibernation module.
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_HIBERNATE);
@@ -35,19 +35,19 @@ int main()
 	HibernateEnableExpClk(SysCtlClockGet());
 	HibernateGPIORetentionEnable(); // continue GPIO state during hibernation.
 	SysCtlDelay(64000000); // delay to allow user to observe the LED.
-	/*
-	// @TODO: After measuring current, uncomment next three lines
+
+	// @TODO: After measuring current for step 17, uncomment next three lines
 	// Configure RTC wakeup parameters
 	HibernateRTCSet(0);			// reset RTC to 0
 	HibernateRTCEnable();		// turn the RTC on and
 	HibernateRTCMatchSet(0, 5); // set wake up time to 5 seconds after RTC = 0
-	*/
+
 	// @TODO: and use
 	// Set wake_pin and RTC as wake condition
-	// HibernateWakeSet(HIBERNATE_WAKE_PIN | HIBERNATE_WAKE_RTC);
+	HibernateWakeSet(HIBERNATE_WAKE_PIN | HIBERNATE_WAKE_RTC);
 
 	// @TODO: Instead of this
-	HibernateWakeSet(HIBERNATE_WAKE_PIN); // set wake_pin as wake condition.
+	//HibernateWakeSet(HIBERNATE_WAKE_PIN); // set wake_pin as wake condition.
 	//@TODO: uncomment next line too.
 	GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0x00); // turn off LED beefore going to sleep.
 
